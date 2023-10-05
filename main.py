a9ad0e5c3afdb74e7abb4ae726d39695e91474d2
dad2jrn <dad2jrn@gmail.com>
2023-10-05 13:24:12 -0400

    starting over to simplify

--- main.py
+++ main.py
@@ -1,64 +1,33 @@
-from pathlib import Path
+# /main.py
 import sys
-import matplotlib.pyplot as plt
+from pathlib import Path
 
-sys.path.append(str(Path(__file__).parent / 'src'))
+sys.path.append(str(Path(__file__).parent / "src"))
 
-from roulette import spin_wheel
-from utils import determine_column, determine_dozen
-from strategies import CustomFibonacciStrategy
-
-def fibonacci_sequence(n):
-    a, b = 0, 1
-    for _ in range(n):
-        a, b = b, a + b
-    return a
-
-def calculate_winnings(number, color, bet_amounts):
-    winnings = 0
-    if color == "Black":
-        winnings += bet_amounts['black'] * 2
-    if determine_column(number) == "2nd":
-        winnings += bet_amounts['2nd_column'] * 3
-    if determine_dozen(number) == "25-36":
-        winnings += bet_amounts['3rd_dozen'] * 3
-    return winnings
+# import local library
+from plot import plot_bankroll_and_bet
+from userbets import simulate_with_user_bets
 
 
-def main():
-    bankroll = 1500
-    num_spins = int(input("Enter the number of spins you'd like to simulate: "))
+# Sample usage:
+bets = [
+    # {"type": "column", "value": 1, "amount": 5},
+    # {"type": "column", "value": 2, "amount": 5},
+    # {"type": "column", "value": 3, "amount": 5},
+    # {"type": "dozen", "value": 1, "amount": 5},
+    # {"type": "dozen", "value": 2, "amount": 5},
+    {"type": "dozen", "value": 3, "amount": 5},
+    # {"type": "color", "value": "red", "amount": 5},
+    # {"type": "color", "value": "black", "amount": 10},
+    # {"type": "zeros", "amount": 5}
+]
+spins = 200
+initial_bankroll = 1000
 
-    strategy = CustomFibonacciStrategy()
+final_bankroll, bankroll_history, bet_history = simulate_with_user_bets(
+    bets, spins, initial_bankroll
+)
+print(f"Final bankroll after {spins} spins: ${final_bankroll}")
 
-    bankroll_history = [bankroll]
-
-    for _ in range(num_spins):
-        number, color = spin_wheel()
-
-        bet_amounts = strategy.calculate_bet(None)  # Initial call to get the base bets
-        total_bet = sum(bet_amounts.values())
-
-        winnings = calculate_winnings(number, color, bet_amounts)
-
-        # Determine outcome of the spin
-        outcome = "win" if winnings > total_bet else "loss"
-
-        # Update strategy with outcome
-        strategy.calculate_bet(outcome)
-
-        bankroll -= total_bet
-        bankroll += winnings
-
-        print(f"Bet Amounts: {bet_amounts}")
-        print(f"Total Bet: ${total_bet}")
-        print(f"Wheel Result: {number} ({color})")
-        print(f"Winnings: ${winnings}")
-        print(f"Remaining Bankroll: ${bankroll}\n")
-
-        bankroll_history.append(bankroll)
-
-
-if __name__ == "__main__":
-    main()
-    exit()
+# Plotting
+plot_bankroll_and_bet(bankroll_history, bet_history)
